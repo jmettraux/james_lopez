7baf21a8034ab17136b05d3907b2b5dc5bfedff4
John Mettraux <jmettraux@gmail.com>
2011-09-28 18:46:39 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+
+gem 'sqlite3'
+
+gem 'mongrel', '1.2.0.pre2' # hopefully '1.2.0.pre2-x86-mingw32'
+gem 'rufus-scheduler', '2.0.10'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamesLopez::Application.config.secret_token = 'f793dd36cfe9c0b9e2462f4a91f32f296d1832d73692532346d10d9cdac1c2041de4334467f9417a08ec68519a708154f73bb62dedb63aad04fe305b565289d9'
